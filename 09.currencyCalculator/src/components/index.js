632a3d9506d916b99803e48e40832c53ff5bfc7f
Ansh Srivastava <42718094+meansh@users.noreply.github.com>
2023-11-21 11:46:08 +0530

    Currency Calculator in ReactJS
    Currency calculator using React

---
+++ 09.currencyCalculator/src/components/index.js
@@ -0,0 +1,5 @@
+//This file is created so that we can import all the components here only, rather than to use export individually every time
+
+import InputBox from "./InputBox";
+
+export {InputBox}
