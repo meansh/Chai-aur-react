1d50edd2c34b06a7fd1fbb8762997d09c23fff3d
Ansh Srivastava <42718094+meansh@users.noreply.github.com>
2024-03-09 22:05:56 +0530

    Joke Generator in React
    It generates a random jokes using the API

---
+++ 22.jokegenerator/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Joke from './Joke';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Joke generator using React and joke API</h1>
+      <Joke/>
+    </div>
+  );
+}
+
+export default App;
